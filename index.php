e48f40dc11729659aa2fc4829bf8dd80d925edbf
Evaëlle PN <epluvinagenierengarten@gmail.com>
2026-04-10 11:49:04 +0200

    index.php
    Exercice commenté en PHP permettant de determiner la moyenne d'éleves

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+    // creation  d'une fonction permetant de recuperer les notes d'un élève dans un tableau
     function CalculerMoyenne($notes){
         $somme=0;
         foreach ($notes as $note){
@@ -8,6 +9,8 @@
         $moyenne = $somme / $nombreDeNotes;
         return $moyenne;
     }
+
+    // creation  d'une fonction permetant de savoir si la moyenne des notes de l'élève permet d'avoir une moyenne suffisante (<10) ou non (>10)
     function afficherResultat($nom,$moyenne){
         if($moyenne >=10){
             echo $nom." a une moyenne suffisante: ".round($moyenne,2);
@@ -16,18 +19,22 @@
         } }
 
 
+    // tableau comprennant les notes de tous les élèves
     $notesAlice = [15, 12, 14, 5, 19, 7, 18, 20];
     $notesGuillaume = [15, 9, 12];
     $notesVeronique = [4, 8, 1, 2];
 
+    // calcul de la moyenne d'Alice, indique si sa moyenne est suffisante
     $moyenneAlice = calculerMoyenne($notesAlice);
     afficherResultat("Alice", $moyenneAlice);
     echo "<br>";
 
+    // calcul de la moyenne d'Alice, indique si sa moyenne est suffisante
     $moyenneGuillaume = calculerMoyenne($notesGuillaume);
     afficherResultat("Guillaume", $moyenneGuillaume);
     echo "<br>";
 
+    // calcul de la moyenne d'Alice, indique si sa moyenne est suffisante
     $moyenneVeronique = calculerMoyenne($notesVeronique);
     afficherResultat("Veronique", $moyenneVeronique);
 ?>
